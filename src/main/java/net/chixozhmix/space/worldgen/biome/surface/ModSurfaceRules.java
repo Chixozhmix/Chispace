e310418c1d7dcb63dabcc6d1b38263553951cca0
Chixozhmix <retrojas635@gmail.com>
2025-07-25 22:13:49 +0700

    Changed Kunabula Surface Rules

--- src/main/java/net/chixozhmix/space/worldgen/biome/surface/ModSurfaceRules.java
+++ src/main/java/net/chixozhmix/space/worldgen/biome/surface/ModSurfaceRules.java
@@ -1,15 +1,34 @@
 package net.chixozhmix.space.worldgen.biome.surface;
 
+import com.github.elenterius.biomancy.init.ModBlocks;
+import net.chixozhmix.space.worldgen.biome.ModBiomes;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 
 public class ModSurfaceRules {
+    private static final SurfaceRules.RuleSource FLESH = makeRules(ModBlocks.FLESH.get());
+    private static final SurfaceRules.RuleSource FIBROUS_FLESH = makeRules(ModBlocks.FIBROUS_FLESH.get());
 
     public static SurfaceRules.RuleSource makeRulesNether() {
         return SurfaceRules.sequence(
-                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR,
-                        SurfaceRules.state(com.github.elenterius.biomancy.init.ModBlocks.FLESH.get().defaultBlockState())),
-                SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR,
-                        SurfaceRules.state(com.github.elenterius.biomancy.init.ModBlocks.FIBROUS_FLESH.get().defaultBlockState()))
+                SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.FLESH_GORGE),
+                        SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, FLESH)),
+                        SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, FIBROUS_FLESH),
+
+                        SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.GASTRIC_GROTTO),
+                                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, FIBROUS_FLESH)),
+                        SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, FIBROUS_FLESH),
+                        SurfaceRules.ifTrue(SurfaceRules.DEEP_UNDER_FLOOR, FIBROUS_FLESH))
+
+
+//                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR,
+//                        SurfaceRules.state(com.github.elenterius.biomancy.init.ModBlocks.FLESH.get().defaultBlockState())),
+//                SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR,
+//                        SurfaceRules.state(com.github.elenterius.biomancy.init.ModBlocks.FIBROUS_FLESH.get().defaultBlockState()))
         );
+    }
+
+    private static SurfaceRules.RuleSource makeRules(Block block) {
+        return SurfaceRules.state(block.defaultBlockState());
     }
 }
