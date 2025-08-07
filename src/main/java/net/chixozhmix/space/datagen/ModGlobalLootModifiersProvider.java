39ef2cb434624a63169ff109bf9f2d03604edf8b
Chixozhmix <retrojas635@gmail.com>
2025-08-07 16:39:40 +0700

    Changed global loot modifier

--- src/main/java/net/chixozhmix/space/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/chixozhmix/space/datagen/ModGlobalLootModifiersProvider.java
@@ -21,12 +21,12 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
     protected void start() {
         add("flesh_orb_from_living_flesh", new AddItemModifiers(new LootItemCondition[]{
                 new LootTableIdCondition.Builder(new ResourceLocation("biomancy:entities/flesh_blob")).build(),
-                LootItemRandomChanceCondition.randomChance(0.1f).build()
+                LootItemRandomChanceCondition.randomChance(0.3f).build()
         }, ModItems.FLESH_ORB.get()));
 
         add("flesh_orb_from_hungry_living_flesh", new AddItemModifiers(new LootItemCondition[]{
                 new LootTableIdCondition.Builder(new ResourceLocation("biomancy:entities/hungry_flesh_blob")).build(),
-                LootItemRandomChanceCondition.randomChance(0.1f).build()
+                LootItemRandomChanceCondition.randomChance(0.3f).build()
         }, ModItems.FLESH_ORB.get()));
     }
 }
